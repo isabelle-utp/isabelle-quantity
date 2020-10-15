07b5159227fddbc84a9fd703a2a860cd5adf83ee
Simon Foster <simon.foster@york.ac.uk>
2020-10-15 11:45:03 +0100

    Some tidying of notation

--- SI_Accepted.thy
+++ SI_Accepted.thy
@@ -4,33 +4,33 @@ theory SI_Accepted
   imports SI_Derived
 begin
 
-definition [si_def, si_eq]: "minute = 60 \<odot> second"
+definition [si_def, si_eq]: "minute = 60 *\<^sub>Q second"
 
-definition [si_def, si_eq]: "hour = 60 \<odot> minute"
+definition [si_def, si_eq]: "hour = 60 *\<^sub>Q minute"
 
-definition [si_def, si_eq]: "day = 24 \<odot> hour"
+definition [si_def, si_eq]: "day = 24 *\<^sub>Q hour"
 
-definition [si_def, si_eq]: "astronomical_unit = 149597870700 \<odot> metre"
+definition [si_def, si_eq]: "astronomical_unit = 149597870700 *\<^sub>Q metre"
 
 definition degree :: "'a::real_field[L/L]" where
-[si_def, si_eq]: "degree = (2\<cdot>(of_real pi) / 180) \<odot> radian"
+[si_def, si_eq]: "degree = (2\<cdot>(of_real pi) / 180) *\<^sub>Q radian"
 
-abbreviation degrees ("_\<degree>" [999] 999) where "n\<degree> \<equiv> n \<odot> degree"
+abbreviation degrees ("_\<degree>" [999] 999) where "n\<degree> \<equiv> n *\<^sub>Q degree"
 
-definition [si_def, si_eq]: "litre = 1/1000 \<odot> metre\<^sup>\<three>"
+definition [si_def, si_eq]: "litre = 1/1000 *\<^sub>Q metre\<^sup>\<three>"
 
-definition [si_def, si_eq]: "tonne = 10^3 \<odot> kilogram"
+definition [si_def, si_eq]: "tonne = 10^3 *\<^sub>Q kilogram"
 
-definition [si_def, si_eq]: "dalton = 1.66053906660 * (1 / 10^27) \<odot> kilogram"
+definition [si_def, si_eq]: "dalton = 1.66053906660 * (1 / 10^27) *\<^sub>Q kilogram"
 
 subsection \<open> Example Unit Equations \<close>
 
-lemma "1 \<odot> hour = 3600 \<odot> second"
+lemma "1 *\<^sub>Q hour = 3600 *\<^sub>Q second"
   by (si_simp)
 
-lemma "watt \<^bold>\<cdot> hour \<cong>\<^sub>Q 3600 \<odot> joule"   by (si_calc)
+lemma "watt \<^bold>\<cdot> hour \<cong>\<^sub>Q 3600 *\<^sub>Q joule"   by (si_calc)
 
-lemma "25 \<odot> metre \<^bold>/ second = 90 \<odot> (kilo \<odot> metre) \<^bold>/ hour"
+lemma "25 *\<^sub>Q metre \<^bold>/ second = 90 *\<^sub>Q (kilo *\<^sub>Q metre) \<^bold>/ hour"
   by (si_calc)
 
 end
--- SI_Imperial.thy
+++ SI_Imperial.thy
@@ -11,64 +11,64 @@ default_sort field_char_0
 text \<open> The units of length are defined in terms of the international yard, as standardised in 1959. \<close>
 
 definition yard :: "'a[L]" where
-[si_eq]: "yard = 0.9144 \<odot> metre"
+[si_eq]: "yard = 0.9144 *\<^sub>Q metre"
 
 definition foot :: "'a[L]" where
-[si_eq]: "foot = 1/3 \<odot> yard"
+[si_eq]: "foot = 1/3 *\<^sub>Q yard"
 
-lemma foot_alt_def: "foot = 0.3048 \<odot> metre"
+lemma foot_alt_def: "foot = 0.3048 *\<^sub>Q metre"
   by (si_simp)
 
 definition inch :: "'a[L]" where
-[si_eq]: "inch = (1 / 36) \<odot> yard"
+[si_eq]: "inch = (1 / 36) *\<^sub>Q yard"
 
-lemma inch_alt_def: "inch = 25.4 \<odot> milli \<odot> metre"
+lemma inch_alt_def: "inch = 25.4 *\<^sub>Q milli *\<^sub>Q metre"
   by (si_simp)
 
 definition mile :: "'a[L]" where
-[si_eq]: "mile = 1760 \<odot> yard"
+[si_eq]: "mile = 1760 *\<^sub>Q yard"
 
-lemma mile_alt_def: "mile = 1609.344 \<odot> metre"
+lemma mile_alt_def: "mile = 1609.344 *\<^sub>Q metre"
   by (si_simp)
 
 definition nautical_mile :: "'a[L]" where
-[si_eq]: "nautical_mile = 1852 \<odot> metre"
+[si_eq]: "nautical_mile = 1852 *\<^sub>Q metre"
 
 subsection \<open> Units of mass \<close>
 
 text \<open> The units of mass are defined in terms of the international yard, as standardised in 1959. \<close>
 
 definition pound :: "'a[M]" where
-[si_eq]: "pound = 0.45359237 \<odot> kilogram"
+[si_eq]: "pound = 0.45359237 *\<^sub>Q kilogram"
 
 definition ounce :: "'a[M]" where
-[si_eq]: "ounce = 1/16 \<odot> pound"
+[si_eq]: "ounce = 1/16 *\<^sub>Q pound"
 
 definition stone :: "'a[M]" where
-[si_eq]: "stone = 14 \<odot> pound"
+[si_eq]: "stone = 14 *\<^sub>Q pound"
 
 subsection \<open> Other units \<close>
 
 definition knot :: "'a[L \<cdot> T\<^sup>-\<^sup>1]" where
-[si_eq]: "knot = 1 \<odot> (nautical_mile \<^bold>/ hour)"
+[si_eq]: "knot = 1 *\<^sub>Q (nautical_mile \<^bold>/ hour)"
 
 definition pint :: "'a[Volume]" where
-[si_eq]: "pint = 0.56826125 \<odot> litre"
+[si_eq]: "pint = 0.56826125 *\<^sub>Q litre"
 
 definition gallon :: "'a[Volume]" where
-[si_eq]: "gallon = 8 \<odot> pint"
+[si_eq]: "gallon = 8 *\<^sub>Q pint"
 
 definition degrees_farenheit :: "'a \<Rightarrow> 'a[\<Theta>]" ("_\<degree>F" [999] 999)
-  where [si_eq]: "degrees_farenheit x = (x + 459.67)\<cdot>5/9 \<odot> kelvin"
+  where [si_eq]: "degrees_farenheit x = (x + 459.67)\<cdot>5/9 *\<^sub>Q kelvin"
 
 default_sort type
 
 subsection \<open> Unit Equations \<close>
   
-lemma miles_to_feet: "mile = 5280 \<odot> foot"
+lemma miles_to_feet: "mile = 5280 *\<^sub>Q foot"
   by si_simp
 
-lemma mph_to_kmh: "1 \<odot> (mile \<^bold>/ hour) = 1.609344 \<odot> ((kilo \<odot> metre) \<^bold>/ hour)"
+lemma mph_to_kmh: "1 *\<^sub>Q (mile \<^bold>/ hour) = 1.609344 *\<^sub>Q ((kilo *\<^sub>Q metre) \<^bold>/ hour)"
   by si_simp
 
 lemma farenheit_to_celcius: "T\<degree>F = ((T - 32) \<cdot> 5/9)\<degree>C"
